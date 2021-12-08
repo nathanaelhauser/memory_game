ac1fe73fa1d1a3ed7c1de69e9296bd7b66226592
Nathanael Hauser <nathanaelhauser@gmail.com>
2021-12-08 02:05:49 -0800

    Prototyped card component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+import { ReactComponent as BeanieSVG } from './assets/svgs/beanie-1619898.svg';
+import cardBackground from './assets/images/card_background.png';
+import "./App.css";
 
 function App() {
+  let [isFlipped, setIsFlipped] = useState(true);
+
   return (
     <div className="App">
-
+      <div className="card-front" onClick={() => setIsFlipped(prevState => !prevState)}>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal" className="card">
+          <div className="card-front">
+            <BeanieSVG />
+          </div>
+          <div className="card-back red-back">
+            <img src={cardBackground} alt="Card Background" className="card-back" />
+          </div>
+        </ReactCardFlip>
+      </div>
     </div>
   );
 }
