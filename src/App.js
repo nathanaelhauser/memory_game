a3260eee0096a4a004999cea327c95b477d377eb
Nathanael Hauser <nathanaelhauser@gmail.com>
2021-12-08 00:39:54 -0800

    Added image assets for memory cards

--- src/App.js
+++ src/App.js
@@ -9,3 +9,6 @@ function App() {
 }
 
 export default App;
+
+
+{/* <a href="https://iconscout.com/icon-pack/christmas-2194" target="_blank">Christmas Icon Pack</a> by <a href="https://iconscout.com/contributors/iconscout" target="_blank">Iconscout Store</a> */ }
